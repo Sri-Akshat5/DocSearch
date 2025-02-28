e0bdc7525b9874b7d4983f2e226f07bca28a948c
Akshat Srivastava <akshatsrivastava566@gmail.com>
2025-02-28 18:45:38 +0530

    Upload db

--- intelligent-doc-search_backend/db.js
+++ intelligent-doc-search_backend/db.js
@@ -1,18 +1,17 @@
-const mysql = require("mysql2");
-const dotenv = require("dotenv");
+const mysql = require('mysql2');
+require('dotenv').config();
 
-dotenv.config();
-
-const db = mysql.createConnection({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
 
-db.connect((err) => {
-  if (err) console.error("Database Connection Failed!", err);
-  else console.log("Connected to MySQL");
+connection.connect((err) => {
+  if (err) {
+    console.error('Database connection failed:', err);
+    return;
+  }
+  console.log('Connected to MySQL Database.');
 });
-
-module.exports = db;
