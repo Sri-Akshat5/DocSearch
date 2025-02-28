7b027484239f1fe8146e70e3ecb45982c162cf2f
Akshat Srivastava <akshatsrivastava566@gmail.com>
2025-02-28 22:52:47 +0530

    update config

--- intelligent-doc-search/src/config.js
+++ intelligent-doc-search/src/config.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = process.env.NODE_ENV === "production" 
-  ? "https://docsearch-production.up.railway.app" 
+  ? "https://docsearch-y8m5.onrender.com" 
   : "http://localhost:5000";
 
 export default API_BASE_URL;
